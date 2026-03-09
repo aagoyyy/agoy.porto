9900cf10d91ac9ed69c7fb38d0d6516914122cee
Yoga Imam <yoga.yoks25@gmail.com>
2026-03-09 15:12:28 +0700

    delete analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { useEffect } from "react";
-import clarity from "react-microsoft-clarity";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-
 export default function App() {
-
-  useEffect(() => {
-    clarity.init("vsyw0z9b2i");
-  }, []);
-
   return (
     <div className="min-h-screen bg-gray-900 text-white font-sans">
-      <Header />
+      <Header/>
       <Home />
     </div>
   );
