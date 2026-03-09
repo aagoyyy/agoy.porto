6a44cfaa873de76f15d61c4dfa1aa9ed1aac5a51
Yoga Imam <yoga.yoks25@gmail.com>
2026-03-09 15:03:46 +0700

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+
 export default function App() {
+
+  useEffect(() => {
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vsyw0z9b2i");
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-900 text-white font-sans">
-      <Header/>
+      <Header />
       <Home />
     </div>
   );
