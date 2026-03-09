5bdb86ebdd814fad24846243a7b56966bab56f55
Yoga Imam <yoga.yoks25@gmail.com>
2026-03-09 15:09:11 +0700

    add clarity analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useEffect } from "react";
+import clarity from "react-microsoft-clarity";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 
 export default function App() {
 
   useEffect(() => {
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "vsyw0z9b2i");
+    clarity.init("vsyw0z9b2i");
   }, []);
 
   return (
